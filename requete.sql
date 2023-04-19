acaa759f88e240294b03f57b4f5a0c8c97310667
qsaval <quentin.saval@hotmail.fr>
2023-04-19 15:32:14 +0200

    modif

--- requete.sql
+++ requete.sql
@@ -6,21 +6,21 @@ JOIN plat ON commande.id_plat = plat.id;
 -- Afficher la liste des plats en spécifiant la catégorie
 SELECT plat.libelle, categorie.libelle
 FROM plat
-JOIN categorie ON categorie.id = plat.categorie_id; 
+JOIN categorie ON categorie.id = plat.id_categorie; 
 
 -- Afficher les catégories et le nombre de plats actifs dans chaque catégorie
 SELECT categorie.libelle, COUNT(plat.libelle) 
 FROM categorie 
-JOIN plat on plat.categorie_id = categorie.id 
+JOIN plat on plat.id_categorie = categorie.id 
 WHERE plat.active = 'Yes'
 GROUP BY categorie.libelle; 
 
 -- Liste des plats les plus vendus par ordre décroissant
 SELECT plat.libelle, COUNT(commande.id) 
 FROM plat 
-JOIN commande ON commande.plat_id = plat.id 
+JOIN commande ON commande.id_plat = plat.id 
 GROUP BY plat.libelle 
-ORDER BY COUNT(commande_plat.commande_id) DESC; 
+ORDER BY COUNT(commande.id) DESC; 
 
 -- Le plat le plus rémunérateur
 SELECT plat.libelle, SUM(commande.total) 
@@ -31,7 +31,7 @@ ORDER BY SUM(commande.total) DESC
 LIMIT 1; 
 
 -- Liste des clients et le chiffre d'affaire généré par client (par ordre décroissant)
-SELECT commande.nom_client, commande.total 
+SELECT commande.nom_client, commande.total * commande.quantite
 FROM commande 
 ORDER BY commande.total DESC;
 
@@ -44,9 +44,11 @@ DELETE FROM commande WHERE commande.etat = 'Livrée'
 -- Ecrivez un script sql permettant d'ajouter une nouvelle catégorie et un plat dans cette nouvelle catégorie.
 use the_district
 
-INSERT INTO categorie (id, libelle, image, active)
+INSERT INTO categorie (id, libelle, image, active) 
+VALUES (20, 'nom1', 'nom.jpg', 'Yes');
 
-INSERT INTO plat (id, categorie_id, libelle, description, prix, image, active)
+INSERT INTO plat (categorie_id, libelle, description, prix, image, active)
+VALUES(20, 'nom2', 'une bonne descrition', 25, 'nom2.jpg', 'Yes')
 
 -- Ecrivez une requête permettant d'augmenter de 10% le prix des plats de la catégorie 'Pizza' 
 UPDATE plat 
